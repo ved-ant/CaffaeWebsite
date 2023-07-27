5c366ba77910ccf19f336d6bff167a15024bccc7
ved-ant <vedantgadodia@gmail.com>
2023-07-27 13:10:31 +0530

    changes made

--- frontend/src/pages/GuruPage/Hero.jsx
+++ frontend/src/pages/GuruPage/Hero.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
           <span className={styl.one}> Become a Guru at Caffae </span>and empower
           numerous individuals to achieve their goals.
         </div>
-        <button className={styl.ctaButton}>Become a Guru</button>
+       <a href="https://forms.gle/9oTcnE91NDdJJWgG8" target="_blank"><button className={styl.ctaButton}>Become a Guru</button></a> 
       </div>
     </Fragment>
   );
