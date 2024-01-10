c3c3ad2be5490fbb113848f54b86da760ce2020d
adityacodes30 <adityaework@gmail.com>
2024-01-11 01:53:26 +0530

    edits on the homepage

---
+++ frontend/src/components/GuruDescSection/DescSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './DescSection.css';
+
+import LeftDesc from '../LeftDesc/LeftDesc';
+import RightDesc from '../RightDesc/RightDesc';
+import Image1 from '../../assets/ellipse3.png';
+import Image2 from '../../assets/search.png';
+import Image3 from '../../assets/ellipse4.png';
+import Image4 from '../../assets/motivation.png';
+import Image5 from '../../assets/ellipse5.png';
+import Image6 from '../../assets/experts.png';
+import Image7 from '../../assets/ellipse6.png';
+import Image8 from '../../assets/verified.png';
+
+const GuruDescSection = () => {
+  return (
+    <div className='desc_section-container'>
+
+        <LeftDesc title="Monetize Your Experience" text="On Caffae your experience will be valued. you can decide your own call charges and earn money per minute for every call you take. " image1={Image3} image2={Image4} />
+        <RightDesc title="Social Backgiving" text="By sharing the experiences and problems you faced, you can give back to the society and help the people in need." image1={Image5} image2={Image6} />
+        <LeftDesc title="Increase Your Network" text="You can expand your Social Network and followers by guiding the freshers through our platform . You can build your reputation and presentation skills" image1={Image7} image2={Image8} />
+    </div>
+  )
+}
+
+export default GuruDescSection;
--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-
+import Infocard from '../../components/Infocard/Infocard';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import DescSection from '../../components/DescSection/DescSection';
+
 import Community from '../../components/Community/Community';
 import Googleform from '../../components/Googleform/Googleform';
+import LeftDesc from '../../components/LeftDesc/LeftDesc';
+import UserDescSection from '../../components/UserDescSection/UserDescSection';
+import GuruDescSection from '../../components/GuruDescSection/DescSection';
 
 const Homepage = () => {
   return (
     <>
         <Header />
-        <DescSection />
+        <LeftDesc title="Caffae is not just an app" text="It's your portal to a global community of shared expertise and inspiration. Our networking software unites individuals worldwide, facilitating connections that transcend distances. Whether exploring a new hobby or seeking professional guidance, Caffae empowers you to connect with like-minded mentors. Navigate a world of knowledge, advice, and collaboration with Caffae – where every connection enriches your journey and elevates your pursuits."></LeftDesc>
+        <Infocard></Infocard>
+      
         <Googleform />
         <Community />
         <Footer />
