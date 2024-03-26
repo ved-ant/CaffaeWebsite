9a3d790e7ef737b2d0f2642e68646da76f10906e
Rushil <ragarwal2_be21@thapar.edu>
2024-03-26 16:07:19 +0530

    styled contact form

--- frontend/src/pages/ContactUs/LeftForm.jsx
+++ frontend/src/pages/ContactUs/LeftForm.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import FormImg from "../../assets/formimg.png";
+import LogoImg from "../../assets/contactLogo.png";
+import LocImg from "../../assets/location.png";
+import PhnImg from "../../assets/phone.png";
+import WebImg from "../../assets/web.png";
+import InsImg from "../../assets/instacolor.png";
+import LinkImg from "../../assets/linkcolor.png";
 import "./leftForm.css";
 
 function LeftForm() {
   return (
     <div className="outline1">
       <img src={FormImg} alt="" className="image1" />
-      <div>
-        <p>Bits Pilani pilani, rajasthan 333031, IN</p>
-        <p>Phone- +91 9518594249</p>
-        <p>Website- caffae.in</p>
+      <img
+        style={{ maxWidth: "175px", position: "relative", top: "-60px" }}
+        src={LogoImg}
+        alt=""
+      />
+      <div style={{ position: "relative", top: "-60px" }}>
+        <p>
+          {" "}
+          <span>
+            <img src={LocImg} alt="" />
+          </span>
+          Bits Pilani pilani, rajasthan 333031, IN
+        </p>
+        <p>
+          <span>
+            <img src={PhnImg} alt="" />
+          </span>
+          Phone- +91 9518594249
+        </p>
+        <p>
+          <span>
+            <img src={WebImg} alt="" />
+          </span>
+          Website- caffae.in
+        </p>
       </div>
+      <span className="socials">
+        <img src={InsImg} alt="" />
+        <img src={LinkImg} alt="" />
+      </span>
     </div>
   );
 }
