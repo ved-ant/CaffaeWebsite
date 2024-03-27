d00b95631a27397ade8bd537d2e815d380428804
Rushil <ragarwal2_be21@thapar.edu>
2024-03-27 23:05:22 +0530

    about us fix

--- frontend/src/components/DescText/DescText.jsx
+++ frontend/src/components/DescText/DescText.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from "react";
+import descImg from "../../assets/descImg.png";
+import "./DescText.css";
 
-import './DescText.css';
-
-const DescText = ({title, text}) => {
+const DescText = ({ title, text }) => {
   return (
-    <div className='desc_text-container'>
-      <div className='desc_text-header'>
-        {title}
-      </div>
-      <div className='desc_text-basetext'>
-        {text}
-      </div>
+    <div className="desc_text-container">
+      <div className="desc_text-header">{title}</div>
+      <div className="desc_text-basetext">{text}</div>
     </div>
-  )
-}
+  );
+};
 
 export default DescText;
--- frontend/src/components/LeftDesc/LeftDesc.jsx
+++ frontend/src/components/LeftDesc/LeftDesc.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import DescText from '../DescText/DescText';
-import './LeftDesc.css';
+import React from "react";
+import DescText from "../DescText/DescText";
+import "./LeftDesc.css";
+import descImg from "../../assets/descImg.png";
 
-const LeftDesc = ({title, text, image1, image2}) => {
+const LeftDesc = ({ title, text, image1, image2 }) => {
   return (
     <div className="left_desc-container">
-      <div className='left_desc-text'>
+      <div className="left_desc-text">
         <DescText title={title} text={text} />
       </div>
-      <div className='left_desc-space'></div>
-      <div className='left_desc-image'>
-        <img src={image1} className='left_desc-bg_ellipse' />
-        <img src={image2} className='left_desc-top_image' />
+      <div className="left_desc-space"></div>
+      <div className="left_desc-image">
+        <img src={image1} className="left_desc-bg_ellipse" />
+        <img src={image2} className="left_desc-top_image" />
       </div>
+      {/* <img src={descImg} alt="" /> */}
     </div>
-  )
-}
+  );
+};
 
 export default LeftDesc;
