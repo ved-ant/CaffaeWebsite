ee9bc65863d907dbbfc09e1a51e76f3d5ef51b38
Rushil <ragarwal2_be21@thapar.edu>
2024-03-27 14:22:22 +0530

    footer update

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,19 +1,19 @@
 import React, { Fragment } from "react";
-import Footer from "./Footer";
+import Footer from "../../components/Footer/Footer";
 import HeroSec from "./HeroSection";
 import Vision from "./vision";
 import Team from "./team";
 import How from "./how";
-const AboutUs = () =>
-{
-    return <div style={{display:'flex', flexDirection:'column' }}>
-    <HeroSec></HeroSec>
-    <Vision></Vision>
-    <Team></Team>
-   <How></How>
-     <Footer></Footer>
-     </div>
-  
-}
+const AboutUs = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <HeroSec></HeroSec>
+      <Vision></Vision>
+      <Team></Team>
+      <How></How>
+      <Footer></Footer>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- frontend/src/pages/GuruPage/GuruPage.jsx
+++ frontend/src/pages/GuruPage/GuruPage.jsx
@@ -2,15 +2,16 @@ import React, { Fragment } from "react";
 import HeroSection from "./Hero";
 import Section1 from "./Section1";
 import Section3 from "./Section2";
-import Footer from "./Footer";
-const GuruPage = () =>
-{
-    return <Fragment>
-        <HeroSection></HeroSection>
-        <Section1></Section1>
-        <Section3></Section3>
-     <Footer></Footer>
+import Footer from "../../components/Footer/Footer";
+const GuruPage = () => {
+  return (
+    <Fragment>
+      <HeroSection></HeroSection>
+      <Section1></Section1>
+      <Section3></Section3>
+      <Footer></Footer>
     </Fragment>
-}
+  );
+};
 
-export default GuruPage
+export default GuruPage;
