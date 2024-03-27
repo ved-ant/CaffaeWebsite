3119ecf9612391145a4ef6c3bcfa0f1a988b9689
Rushil <ragarwal2_be21@thapar.edu>
2024-03-27 12:28:36 +0530

    fix navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,68 +1,78 @@
-import { useState } from 'react'
-import  styles from './Navbar.module.css';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/contactLogo.png";
 
 function Navbar() {
-
-
-  // adding the states 
+  // adding the states
   const [isActive, setIsActive] = useState(false);
-
 
   //add the active class
   const toggleActiveClass = () => {
     setIsActive(!isActive);
   };
 
-
   //clean up function to remove the active class
   const removeActive = () => {
-    setIsActive(false)
-  }
-
+    setIsActive(false);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-
-
         <nav className={`${styles.navbar}`}>
-
-
           {/* logo */}
-          <a href='/' className={`${styles.logo}`}>Caffae</a>
+          <a href="/" className={`${styles.logo}`}>
+            <img src={Logo} alt="" />
+          </a>
 
-
-          <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
-            <li onClick={removeActive}>
-              <a href='/' className={`${styles.navLink}`}>HOME</a>
+          <ul className={`${styles.navMenu}`}>
+            <li
+            // onClick={toggleActiveClass}
+            // className={`${isActive ? styles.active : ""}`}
+            >
+              <a href="/" className={`${styles.navLink}`}>
+                HOME
+              </a>
             </li>
-            <li onClick={removeActive}>
-              <a href='/aboutus' className={`${styles.navLink}`}>ABOUT US</a>
+            <li
+            // onClick={toggleActiveClass}
+            // className={`${isActive ? styles.active : ""}`}
+            >
+              <a href="/aboutus" className={`${styles.navLink}`}>
+                ABOUT US
+              </a>
             </li>
-            <li onClick={removeActive}>
-              <a href='/guru' className={`${styles.navLink}`}>BECOME A GURU</a>
+            <li
+            // onClick={toggleActiveClass}
+            // className={`${isActive ? styles.active : ""}`}
+            >
+              <a href="/guru" className={`${styles.navLink}`}>
+                BECOME A GURU
+              </a>
             </li>
-            <li onClick={removeActive}>
-              <a href='/contactus' className={`${styles.navLink}`}>CONTACT US</a>
+            <li
+            // onClick={toggleActiveClass}
+            // className={`${isActive ? styles.active : ""}`}
+            >
+              <a href="/contactus" className={`${styles.navLink}`}>
+                CONTACT US
+              </a>
             </li>
           </ul>
 
-
-          <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  onClick={toggleActiveClass}>
+          <div
+            className={`${styles.hamburger} ${isActive ? styles.active : ""}`}
+            onClick={toggleActiveClass}
+          >
             <span className={`${styles.bar}`}></span>
             <span className={`${styles.bar}`}></span>
             <span className={`${styles.bar}`}></span>
           </div>
         </nav>
-
-
       </header>
     </div>
   );
 }
 
-
 export default Navbar;
-
-;
