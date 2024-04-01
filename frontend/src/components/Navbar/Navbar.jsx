6259a169c11ac8ab4a59369f0897b2657c406b29
Rushil <ragarwal2_be21@thapar.edu>
2024-04-01 16:13:16 +0530

    header fix

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,26 +2,42 @@ import { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/contactLogo.png";
+import { click } from "@testing-library/user-event/dist/click";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+  // const handleClick = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
-  // adding the states
-  const [isActive, setIsActive] = useState(false);
+  // // adding the states
+  // const [isActive, setIsActive] = useState(false);
 
-  //add the active class
-  const toggleActiveClass = () => {
-    setIsActive(!isActive);
-  };
+  // //add the active class
+  // const toggleActiveClass = () => {
+  //   setIsActive(!isActive);
+  // };
 
-  //clean up function to remove the active class
-  const removeActive = () => {
-    setIsActive(false);
-  };
+  // //clean up function to remove the active class
+  // const removeActive = () => {
+  //   setIsActive(false);
+  // };
+
+  const [clicked, setClicked] = useState(false);
+
+  function handleHamburger() {
+    if (clicked === false) setClicked(true);
+    else setClicked(false);
+
+    const navmenu = document.getElementById("navmen");
+    const ham = document.getElementById("ham");
+
+    navmenu.classList.toggle("active");
+    ham.classList.toggle("active");
+
+    console.log("works");
+  }
 
   return (
     <div className="App">
@@ -32,7 +48,7 @@ function Navbar() {
             <img src={Logo} alt="" />
           </a>
 
-          <ul className={`${styles.navMenu}`}>
+          <ul className={`${styles.navMenu}`} id="navmen">
             <li
             // onClick={toggleActiveClass}
             // className={`${isActive ? styles.active : ""}`}
@@ -66,9 +82,15 @@ function Navbar() {
               </a>
             </li>
           </ul>
-          
-
-
+          <div
+            className={`${styles.hamburger}`}
+            onClick={handleHamburger}
+            id="ham"
+          >
+            <span className={`${styles.bar}`}></span>
+            <span className={`${styles.bar}`}></span>
+            <span className={`${styles.bar}`}></span>
+          </div>
         </nav>
       </header>
     </div>
